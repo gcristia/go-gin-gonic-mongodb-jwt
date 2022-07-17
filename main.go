34465aaed2e6462f04e146ec178a798960212f44
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2022-07-17 02:42:02 -0400

    Add Project

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"gin+gonic+mongodb+jwt/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	err = router.Run(":" + port)
+	if err != nil {
+		return
+	}
+}
---
+++ routes/userRouter.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"gin+gonic+mongodb+jwt/controllers"
+	"gin+gonic+mongodb+jwt/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controllers.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
+}
